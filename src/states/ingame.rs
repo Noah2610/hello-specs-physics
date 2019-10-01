e97e1a02205cb72fb7a4c4fe943c72eb6b033af2
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2019-10-02 01:37:40 +0200

    Trying to implement Menu for Ingame state

--- src/states/ingame.rs
+++ src/states/ingame.rs
@@ -1,11 +1,17 @@
 use super::state_prelude::*;
 
+use deathframe::menu::{Menu, UiData};
+
+const UI_RON_PATH: &str = "resources/ui/ingame.ron";
+
 #[derive(Default)]
-pub struct Ingame {}
+pub struct Ingame {
+    ui_data: UiData,
+}
 
 impl<'a, 'b> State<CustomGameData<'a, 'b, ()>, StateEvent> for Ingame {
     fn on_start(&mut self, mut data: StateData<CustomGameData>) {
-        add_resources(&mut data);
+        self.create_ui(&mut data);
     }
 
     fn update(
@@ -16,9 +22,41 @@ impl<'a, 'b> State<CustomGameData<'a, 'b, ()>, StateEvent> for Ingame {
 
         Trans::None
     }
+
+    // TODO
+    // fn fixed_update(
+    //     &mut self,
+    //     mut data: StateData<CustomGameData<()>>,
+    // ) -> Trans<CustomGameData<'a, 'b, ()>, StateEvent> {
+    //     if let Some(trans) = self.update_ui_events(&mut data) {
+    //         return trans;
+    //     }
+    //     Trans::None
+    // }
 }
 
-fn add_resources(data: &mut StateData<CustomGameData>) {
-    // add_resource_display_config(data);
-    // add_resource_screen_dimensions(data); // NOTE: Has to be called _after_ DisplayConfig resource is added.
+impl<'a, 'b> Menu<CustomGameData<'a, 'b, ()>, StateEvent> for Ingame {
+    fn event_triggered(
+        &mut self,
+        data: &mut StateData<CustomGameData<'a, 'b, ()>>,
+        event_name: String,
+    ) -> Option<Trans<CustomGameData<'a, 'b, ()>, StateEvent>> {
+        match event_name.as_ref() {
+            "btn_primary" => None,
+            "btn_secondary" => Some(Trans::Pop),
+            _ => None,
+        }
+    }
+
+    fn ui_ron_path(&self) -> &str {
+        UI_RON_PATH
+    }
+
+    fn ui_data(&self) -> &UiData {
+        &self.ui_data
+    }
+
+    fn ui_data_mut(&mut self) -> &mut UiData {
+        &mut self.ui_data
+    }
 }
