01a287569ae528b126edee9e876ac4a77bad083d
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2019-10-02 20:49:26 +0200

    Use UiBundle, try to load UI from resources/ui/ingame.ron
    The syntax for the UI ron files probably changed.

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ mod states;
 use amethyst::core::transform::TransformBundle;
 use amethyst::renderer::types::DefaultBackend;
 use amethyst::renderer::{RenderFlat2D, RenderToWindow, RenderingBundle};
+use amethyst::ui::{DrawUi, UiBundle};
 use amethyst::utils::application_root_dir;
 use amethyst::{ApplicationBuilder, LogLevelFilter, LoggerConfig};
 use deathframe::custom_game_data::prelude::*;
@@ -51,18 +52,17 @@ fn build_game_data<'a, 'b>(
                 .with_clear([0.0, 0.0, 0.0, 1.0]),
         )
         .with_plugin(RenderFlat2D::default());
+    // .with_plugin(DrawUi::new());
     let transform_bundle = TransformBundle::new();
     let input_bundle = input::input_bundle();
+    let ui_bundle = UiBundle::<input::Bindings>::new();
 
     let custom_game_data = CustomGameDataBuilder::<'a, 'b, ()>::default()
-        .dispatcher("ingame")
-        .unwrap()
-        .with_core_bundle(rendering_bundle)
-        .unwrap()
-        .with_core_bundle(transform_bundle)
-        .unwrap()
-        .with_core_bundle(input_bundle)
-        .unwrap();
+        .dispatcher("ingame")?
+        .with_core_bundle(rendering_bundle)?
+        .with_core_bundle(transform_bundle)?
+        .with_core_bundle(input_bundle)?
+        .with_core_bundle(ui_bundle)?;
 
     Ok(custom_game_data)
 }
--- src/states/ingame.rs
+++ src/states/ingame.rs
@@ -1,17 +1,20 @@
 use super::state_prelude::*;
 
+use amethyst::assets::ProgressCounter;
+use amethyst::utils::app_root_dir::application_dir;
 use deathframe::menu::{Menu, UiData};
 
 const UI_RON_PATH: &str = "resources/ui/ingame.ron";
 
 #[derive(Default)]
 pub struct Ingame {
-    ui_data: UiData,
+    ui_data:     UiData,
+    ui_progress: Option<ProgressCounter>,
 }
 
 impl<'a, 'b> State<CustomGameData<'a, 'b, ()>, StateEvent> for Ingame {
     fn on_start(&mut self, mut data: StateData<CustomGameData>) {
-        self.create_ui(&mut data);
+        self.ui_progress = Some(self.create_ui(&mut data));
     }
 
     fn update(
@@ -23,16 +26,23 @@ impl<'a, 'b> State<CustomGameData<'a, 'b, ()>, StateEvent> for Ingame {
         Trans::None
     }
 
-    // TODO
-    // fn fixed_update(
-    //     &mut self,
-    //     mut data: StateData<CustomGameData<()>>,
-    // ) -> Trans<CustomGameData<'a, 'b, ()>, StateEvent> {
-    //     if let Some(trans) = self.update_ui_events(&mut data) {
-    //         return trans;
-    //     }
-    //     Trans::None
-    // }
+    fn fixed_update(
+        &mut self,
+        mut data: StateData<CustomGameData<'a, 'b, ()>>,
+    ) -> Trans<CustomGameData<'a, 'b, ()>, StateEvent> {
+        if let Some(trans) = self.update_ui_events(&mut data) {
+            return trans;
+        }
+
+        // Print ui progress errors
+        if let Some(progress) = self.ui_progress.as_ref() {
+            for error in progress.errors() {
+                eprintln!("{:#?}", error);
+            }
+        }
+
+        Trans::None
+    }
 }
 
 impl<'a, 'b> Menu<CustomGameData<'a, 'b, ()>, StateEvent> for Ingame {
@@ -41,6 +51,8 @@ impl<'a, 'b> Menu<CustomGameData<'a, 'b, ()>, StateEvent> for Ingame {
         data: &mut StateData<CustomGameData<'a, 'b, ()>>,
         event_name: String,
     ) -> Option<Trans<CustomGameData<'a, 'b, ()>, StateEvent>> {
+        dbg!(&event_name);
+
         match event_name.as_ref() {
             "btn_primary" => None,
             "btn_secondary" => Some(Trans::Pop),
@@ -48,8 +60,13 @@ impl<'a, 'b> Menu<CustomGameData<'a, 'b, ()>, StateEvent> for Ingame {
         }
     }
 
-    fn ui_ron_path(&self) -> &str {
-        UI_RON_PATH
+    fn ui_ron_path(&self) -> String {
+        application_dir(UI_RON_PATH)
+            .expect("Should have root dir")
+            .into_os_string()
+            .to_str()
+            .expect("Should have root dir")
+            .to_string()
     }
 
     fn ui_data(&self) -> &UiData {
